3465d2c2670e069b2594975e8b76b0e206f2c7fd
Leonard <leonard.sellem@gmail.com>
2019-04-08 18:22:34 +0200

    add prices, cuts and revenue model

---
+++ script.py
@@ -0,0 +1,45 @@
+hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
+
+prices = [30, 25, 40, 20, 20, 35, 50, 35]
+
+last_week = [2, 3, 5, 8, 4, 4, 6, 2]
+
+
+#Prices and Cuts:
+
+total_price = 0
+
+#for price in prices:
+#    total_price += price
+
+#average_price = total_price / len(prices)
+
+#print("Average Haircut Price: " + str(average_price))
+
+new_prices = [price - 5 for price in prices]
+
+print(new_prices)
+
+for price in new_prices:
+    total_price += price
+
+new_average_price = total_price / len(new_prices)
+
+print("New average Haircut Price: " + str(new_average_price))
+
+#Revenue
+
+total_revenue = 0
+
+for i in range(len(hairstyles)):
+    total_revenue += prices[i] * last_week[i]
+
+print("Total Revenue: " + str(total_revenue))
+
+average_daily_revenue = total_revenue / 7
+
+print(average_daily_revenue)
+
+cuts_under_30 = [hairstyles[i] for i in range(len(new_prices)) if new_prices[i] < 30]
+
+print(cuts_under_30)
